41e6383b53256b040b77900614f3ea2f46c2776a
llt3055 <llt30555@gmail.com>
2026-03-16 02:54:43 +0800

    add cotent in Goat class

--- lab21.cpp
+++ lab21.cpp
@@ -3,3 +3,25 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
+
+using namespace std;
+
+class Goat {
+public:
+// Data members
+    int age;
+    string name;
+    string color;
+//genrate random goat data by replit ai
+    string names[15] = {
+        "Billy", "Nanny", "Goaty", "Cloud", "Snow", 
+        "Shadow", "Giddy", "Butter", "Pepper", "Salt", 
+        "Brownie", "Cookie", "Dancer", "Prancer", "Vixen"
+    };
+
+    string colors[15] = {
+        "White", "Black", "Red", "Gold", "Mauve", 
+        "Yellow", "Gray", "Silver", "Pink", "Blue", 
+        "Green", "Orange", "Purple", "Cyan", "Tan"
+    };
+}
